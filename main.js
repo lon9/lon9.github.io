e522091a2148e462661d1a1bb2825e6a19112d5d
lon9 <rompei0924@gmail.com>
2018-06-17 18:09:30 +0900

    Add github-info.

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ $(function(){
         }else{
           $('.title').append(' (' + count + ')');
         }
-    });
+      });
   }
 
   function addElem(repo){
@@ -39,5 +39,50 @@ $(function(){
       elem = '<li><a target="_blank" href="' + repo.html_url + '">' + repo.name + '</a> - ' + repo.language + '</li>';
     }
     $('.repos').append(elem);
+  }
+
+  fetch('/github-info/languages.json')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      data = data.filter(lang => { 
+        if(lang.name == 'PostScript' || lang.name == 'HTML' || lang.name == 'CSS' || lang.name == 'Jupyter Notebook') return false;
+        return true;
+      });
+      data.sort((a,b) => {
+        if(a.bytes < b.bytes){
+          return 1;
+        }else if(a.bytes > b.bytes){
+          return -1;
+        }
+        return 0;
+      });
+      var sum = data.reduce((prevLang, currLang) => {
+        return {
+          bytes: prevLang.bytes + currLang.bytes
+        }
+      });
+      var chartData = {
+        datasets: [{
+          data: [],
+          backgroundColor: []
+        }],
+        labels: []
+      };
+      data.map(lang => {
+        chartData.datasets[0].data.push(((lang.bytes/sum.bytes)*100).toFixed(2));
+        chartData.datasets[0].backgroundColor.push('rgba(' + getRandomInt(256) + ',' + getRandomInt(256) + ',' + getRandomInt(256) + ', 1)');
+        chartData.labels.push(lang.name);
+      });
+      var ctx = document.getElementById('lang-pie').getContext('2d');
+      var langC = new Chart(ctx, {
+        type: 'doughnut',
+        data: chartData
+      });
+    });
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random()*Math.floor(max));
   }
 });
