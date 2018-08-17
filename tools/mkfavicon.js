c6de8a58f1a4683f86d8a6b5aef50781138cc15e
lon9 <rompei0924@gmail.com>
2018-08-17 14:12:56 +0900

    Add function to convert not png images.

--- tools/mkfavicon.js
+++ tools/mkfavicon.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const axios = require('axios');
 const toIco = require('to-ico');
+const sharp = require('sharp');
 
 const main = async () =>  {
   let res = await axios.get(`https://api.github.com/users/${process.env.USER_NAME}`);
@@ -13,8 +14,11 @@ const main = async () =>  {
       's': '256'
     }
   }).catch((err) => console.error(err));
-  const files = [Buffer.from(res.data)];
-  const buf = await toIco(files, {
+
+  const img =  await sharp(Buffer.from(res.data))
+    .png()
+    .toBuffer()
+  const buf = await toIco([img], {
     resize: true
   });
   fs.writeFileSync('static/favicon.ico', buf, 'binary');
