4c14255cecfa815ade150c5835ef555e96f224ee
lon9 <rompei0924@gmail.com>
2018-08-17 17:15:30 +0900

    Modify to use components.

---
+++ components/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h4>Profile</h4>
+  <a target="_blank" :href="user.html_url"><img :src="user.avatar_url" width="128" height="128"></a>
+  <p>
+    <span v-if="user.name">Name: {{ user.name }}<br></span>
+    <span v-if="user.bio">Bio: {{ user.bio }}<br></span>
+    <span v-if="user.location">Location: {{ user.location }}<br></span>
+    <span v-if="user.email">Email: {{ user.email }}<br></span>
+    <span v-if="user.company">Company: {{ user.company }}<br></span>
+    <span v-if="user.blog">URL: {{ user.blog }}</span>
+  </p>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['user']
+}
+</script>
---
+++ components/Repositories.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4>{{ title }} ({{ repos.length }})</h4>
+    <ul>
+      <li v-for="repo in repos" :key="repo.id">
+        <a target="_blank" :href="repo.html_url">{{ repo.name }}</a><span v-if="repo.language || repo.description"> - </span><font :color="languages[repo.language] != undefined ? languages[repo.language].color : '#000000'">{{ repo.language }}</font> {{ repo.description }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title', 'repos', 'languages']
+}
+</script>
