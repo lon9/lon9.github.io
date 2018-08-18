94d46b0758862e6ba906845eab524c6641df9955
lon9 <rompei0924@gmail.com>
2018-08-18 14:05:58 +0900

    Refactor.

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,14 +1,13 @@
 <template>
 <div>
-  <h4>Profile</h4>
-  <a target="_blank" :href="user.html_url"><img :src="user.avatar_url" width="128" height="128"></a>
+  <a target="_blank" :href="user.html_url"><img :src="user.avatar_url" width="128" height="128" alt="avatar"></a>
   <p>
     <span v-if="user.name">Name: {{ user.name }}<br></span>
     <span v-if="user.bio">Bio: {{ user.bio }}<br></span>
     <span v-if="user.location">Location: {{ user.location }}<br></span>
     <span v-if="user.email">Email: {{ user.email }}<br></span>
     <span v-if="user.company">Company: {{ user.company }}<br></span>
-    <span v-if="user.blog">URL: {{ user.blog }}</span>
+    <span v-if="user.blog">URL: <a target="_blank" :href="user.blog">{{ user.blog }}</a></span>
   </p>
 </div>
 </template>
